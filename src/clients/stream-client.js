e10aa2653160bd0d15a220bdab8f91cedee2cc0e
Gene Hallman <gene@livefyre.com>
2013-03-15 12:15:13 -0700

    added some tests and comments

--- src/clients/stream-client.js
+++ src/clients/stream-client.js
@@ -1,20 +1,39 @@
 define(['jquery'], function($) {
-
+    
+    /**
+     * Stream client connects to livefyre's stream server will fetch content objects
+     * from the stream.
+     * @param opts {Object} A set of options to config the stream client with
+     * @param opts.network {string} The name of the network in the livefyre platform
+     * @param opts.siteId {string} The livefyre siteId for the conversation stream
+     * @param opts.commentId {string?} The commentId to start streaming from (default "0")
+     * @constructor
+     */
     var StreamClient = function(opts) {
         opts = opts || {};
         this.network = opts.network || "livefyre.com";
         this.siteId = opts.siteId;
-        this.commentId = opts.commentId || 0;
+        this.commentId = opts.commentId || "0";
+        this._commentId = this.commentId;
         this._urlBase = "http://stream1." + this.network + "/v3.0/collection/" + this.siteId + "/";
     };
     
+    /**
+     * Fetches content from the livefyre conversation stream that this client
+     * has been configured with.
+     * @param callback {function} A callback that is called upon success/failure of the
+     * stream request. Callback signature is "function(error, data)". 
+     */
     StreamClient.prototype.load = function(callback) {
+        callback = callback || function() {};
         var url = this._urlBase + this.commentId + "/";
+        var self = this;
 
-        $.get(url, function(data) {
-            console.log("success", arguments);
-        }).fail(function(err) {
-            console.log("error", arguments);
+        $.getJSON(url, function(data, status, jqXhr) {
+            self.commentId = data.data.maxEventId + ""; 
+            callback(null, data.data);
+        }).fail(function(jqXhr, status, err) {
+            callback(err);
         });
     };
     
--- tests/spec/clients/stream-client.js
+++ tests/spec/clients/stream-client.js
@@ -1,7 +1,9 @@
 define([
-    'jasmine-jquery',
-    'streamhub-sdk/clients/stream-client'],
-function (jasmine, StreamClient) {
+    'jquery',
+    'jasmine',
+    'streamhub-sdk/clients/stream-client',
+    'jasmine-jquery'],
+function ($, jasmine, StreamClient) {
 	describe('A StreamClient', function () {
 		
 		// construction behavior
@@ -22,13 +24,6 @@ function (jasmine, StreamClient) {
 		    var client;
 		    
 		    beforeEach(function() {
-		        setFixtures(
-			        '<div id="hub-ScorecardView">'+
-			        '<div class="score1"></div>'+
-	  				'<div class="score2"></div>'+
-	  				'<div class="quarter"></div>'+
-			        '</div>'
-			    );
 		        client = new StreamClient({
                     "network": "labs.fyre.co",
                     "siteId": "33129653",
@@ -36,8 +31,31 @@ function (jasmine, StreamClient) {
                 });
 			});
 	        it ("should connect to a remote collection", function () {
-	        	client.load();
-	        	expect(client).toBeDefined();
+                var mockData = {"states":{"tweet-312328006913904641@twitter.com":{"vis":1,"content":{"replaces":"","bodyHtml":"<a vocab=\"http://schema.org\" typeof=\"Person\" rel=\"nofollow\" resource=\"acct:14268796\" data-lf-handle=\"\" data-lf-provider=\"twitter\" property=\"url\" href=\"https://twitter.com/#!/TheRoyalty\" target=\"_blank\" class=\"fyre-mention fyre-mention-twitter\">@<span property=\"name\">TheRoyalty</span></a> hoppin on a green frog after the set at <a vocab=\"http://schema.org\" typeof=\"Person\" rel=\"nofollow\" resource=\"acct:1240466234\" data-lf-handle=\"\" data-lf-provider=\"twitter\" property=\"url\" href=\"https://twitter.com/#!/Horseshoe_SX13\" target=\"_blank\" class=\"fyre-mention fyre-mention-twitter\">@<span property=\"name\">Horseshoe_SX13</span></a> showcase during <a href=\"https://twitter.com/#!/search/realtime/%23sxsw\" class=\"fyre-hashtag\" hashtag=\"sxsw\" rel=\"tag\" target=\"_blank\">#sxsw</a> <a href=\"http://t.co/lUqA5TT7Uy\" target=\"_blank\" rel=\"nofollow\">pic.twitter.com/lUqA5TT7Uy</a>","annotations":{},"authorId":"190737922@twitter.com","parentId":"","updatedAt":1363299774,"id":"tweet-312328006913904641@twitter.com","createdAt":1363299774},"source":1,"lastVis":0,"type":0,"event":1363299777181024},"oem-3-tweet-312328006913904641@twitter.com":{"vis":1,"content":{"targetId":"tweet-312328006913904641@twitter.com","authorId":"-","link":"http://twitter.com/PlanetLA_Music/status/312328006913904641/photo/1","oembed":{"provider_url":"http://twitter.com","title":"Twitter / PlanetLA_Music: @TheRoyalty hoppin on a green ...","url":"","type":"rich","html":"<blockquote class=\"twitter-tweet\"><a href=\"https://twitter.com/PlanetLA_Music/status/312328006913904641\"></a></blockquote><script async src=\"//platform.twitter.com/widgets.js\" charset=\"utf-8\"></script>","author_name":"","height":0,"thumbnail_width":568,"width":0,"version":"1.0","author_url":"","provider_name":"Twitter","thumbnail_url":"https://pbs.twimg.com/media/BFWcquJCUAA7orG.jpg","thumbnail_height":568},"position":3,"id":"oem-3-tweet-312328006913904641@twitter.com"},"source":1,"lastVis":0,"type":3,"event":1363299777193595}},"authors":{"190737922@twitter.com":{"displayName":"PlanetLA_Music","tags":[],"profileUrl":"https://twitter.com/#!/PlanetLA_Music","avatar":"http://a0.twimg.com/profile_images/1123786999/PLAnew-logo_normal.jpg","type":3,"id":"190737922@twitter.com"}},"jsver":"10026","maxEventId":1363299777193595};
+                console.log(mockData);
+                var spy = spyOn($, "getJSON").andCallFake(function(url, cb) {
+                    cb({"data": mockData});
+                });
+                spy.fail = function() {};
+                
+                var callback = jasmine.createSpy();
+	        	client.load(callback);
+
+	        	waitsFor(function() {
+			        return callback.callCount > 0;
+			    });
+			    runs(function() {
+			        window.gene = callback.mostRecentCall.args[1];
+			        console.log(window.gene);
+			        expect(callback).toHaveBeenCalled();
+			        expect(callback.callCount).toBe(1);
+			        expect(callback.mostRecentCall.args[0]).toBeNull();
+                    expect(callback.mostRecentCall.args[1]).toBeDefined();
+                    expect(callback.mostRecentCall.args[1].states).toBeDefined();
+                    expect(Object.keys(callback.mostRecentCall.args[1].states).length).toBeGreaterThan(0);
+                    expect(callback.mostRecentCall.args[1].authors).toBeDefined();
+                    expect(Object.keys(callback.mostRecentCall.args[1].authors).length).toBeGreaterThan(0);
+			    });				        	
 	        });
 	    });
 	}); 
