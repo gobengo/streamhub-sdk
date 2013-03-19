508f807c5ded17588c493001630346ea4113e8ab
Gene Hallman <gene@livefyre.com>
2013-03-19 16:52:50 -0700

    added another test

--- tests/spec/stream.js
+++ tests/spec/stream.js
@@ -5,11 +5,13 @@ define([
     'jasmine-jquery'],
 function ($, jasmine, Stream) {
     describe('A base Stream', function () {
-        var stream;
+        var stream, opts;
         
         beforeEach(function() {
             stream = new Stream();
             stream._read = jasmine.createSpy();
+            stream._write = jasmine.createSpy();
+            opts = {"asdf": "blah"};
         });
 
         it ("should call _read when start() is called", function () {
@@ -22,6 +24,20 @@ function ($, jasmine, Stream) {
                 expect(stream._read).toHaveBeenCalled();
                 expect(stream._read.callCount).toBe(1);
             });                            
+        });
+
+        it ("should call _write when write() is called", function () {
+            stream.write(opts);
+    
+            waitsFor(function() {
+                return stream._write.callCount > 0;
+            });
+            runs(function() {
+                expect(stream._write).toHaveBeenCalled();
+                expect(stream._write.callCount).toBe(1);
+                expect(stream._write.mostRecentCall.args.length).toBe(1);
+                expect(stream._write.mostRecentCall.args[0]).toBe(opts);
+            });
         });
     }); 
 });
