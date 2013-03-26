1d0ba5935590cab1e3f19e49d33470acfd20c6c6
Gene Hallman <gene@livefyre.com>
2013-03-26 16:35:09 -0700

    added livefyre content stream

---
+++ src/streams/livefyre-content-stream.js
@@ -0,0 +1,53 @@
+define(['jquery',
+    'streamhub-sdk/stream',
+    'streamhub-sdk/streams/livefyre-stream'
+], function($, Stream, LivefyreStream) {
+
+    /**
+     * Defines a livefyre content stream that turns livefyre emitted content into
+     * a standard content format, and handles update events on the stream.
+     * @param opts {Object} A set of options to config the stream client with
+     * @constructor
+     */
+    var LivefyreContentStream = function(opts) {
+        Stream.call(this);
+        var self = this;
+
+        this.stream = new LivefyreStream(opts);
+        this.stream.on('readable', function() {
+            self._readStream();
+        });
+        this.stream.on('end', function() {
+            self.emit('end');
+        });
+    };
+    $.extend(LivefyreContentStream.prototype, Stream.prototype);
+    
+    /**
+     * Read an object from the underlying stream and convert it to content object
+     * @private
+     */
+    LivefyreContentStream.prototype._readStream = function() {
+        var content = this.stream.read();
+        this._push(content);
+    };
+    
+    /**
+     * Streams content objects from the Livefyre stream's data.
+     * @private
+     */
+    LivefyreContentStream.prototype._read = function() {
+        this.stream._read();
+    };
+    
+    /**
+     * Writes content to the underlying Livefyre stream.
+     * @param opts {Object} Options to pass to the LivefyreStream
+     * @private
+     */
+    LivefyreContentStream.prototype._write = function(opts) {
+        this.stream._write(opts);
+    };
+
+    return LivefyreContentStream;
+});
