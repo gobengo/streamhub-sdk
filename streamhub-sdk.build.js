b00b6d1bf7b4225cf61fa10acd541302f985323c
Gene Hallman <gene@livefyre.com>
2013-03-28 09:58:16 -0700

    include mini zepto in build instead of jquery

--- streamhub-sdk.build.js
+++ streamhub-sdk.build.js
@@ -1,12 +1,12 @@
 ({
   paths: {
-    jquery: 'lib/jquery/jquery',
+    jquery: 'lib/zepto/zepto.min',
     base64: 'lib/base64/base64',
     'streamhub-sdk': 'src'
   },
   baseUrl: '.',
   name: "src/main",
-  exclude: ["jquery"],
+  //exclude: ["jquery"],
   out: "streamhub-sdk.built.js",
   optimize: "uglify2",
   uglify2: {
