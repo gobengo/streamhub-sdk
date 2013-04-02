1a78bd33240b40b4758ff06807be5091406da9ac
Benjamin Goering <bengoering@gmail.com>
2013-04-02 09:04:27 -0700

    Add missing comma in requirejs.conf.js

--- requirejs.conf.js
+++ requirejs.conf.js
@@ -5,7 +5,7 @@ require.config({
     text: 'lib/requirejs-text/text',
     base64: 'lib/base64/base64',
     hogan: 'lib/hogan/web/builds/2.0.0/hogan-2.0.0.amd',
-    hgn: 'lib/requirejs-hogan-plugin/hgn'
+    hgn: 'lib/requirejs-hogan-plugin/hgn',
     jasmine: 'lib/jasmine/lib/jasmine-core/jasmine',
     'jasmine-html': 'lib/jasmine/lib/jasmine-core/jasmine-html',
     'jasmine-jquery': 'lib/jasmine-jquery/lib/jasmine-jquery',
