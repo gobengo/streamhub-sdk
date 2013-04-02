5f71f2963b0f913263d9b90af34cb4bdcf3f77c1
Benjamin Goering <bengoering@gmail.com>
2013-04-01 21:04:29 -0700

    Test TwitterSearchContent, TwitterStreamingContent, subclass TwitterContent for both, and add to ContentView examples

--- src/content/types/twitter-search-content.js
+++ src/content/types/twitter-search-content.js
@@ -1,18 +1,19 @@
 define([
-    'jquery',
-    'streamhub-sdk/content/content',
-], function($, Content) {
+    'streamhub-sdk/content/types/twitter-content'
+], function(TwitterContent) {
     var TwitterSearchContent = function (json) {
-        Content.call(this);
+        TwitterContent.call(this);
         this.tweetId = json.id_str;
         this.author = {
             twitterUserId: json.from_user_id_str,
             displayName: json.from_user,
-            name: json.from_user_name
+            name: json.from_user_name,
+            avatar: json.profile_image_url
         };
         this.htmlString = json.text;
-    }
-    $.extend(TwitterSearchContent.prototype, Content.prototype);
-     
+        this.createdAt = new Date(json.created_at);
+    };
+    TwitterSearchContent.prototype = new TwitterContent();
+
     return TwitterSearchContent;
  });
--- src/content/types/twitter-streaming-content.js
+++ src/content/types/twitter-streaming-content.js
@@ -1,18 +1,20 @@
 define([
     'jquery',
-    'streamhub-sdk/content/content',
-], function($, Content) {
+    'streamhub-sdk/content/types/twitter-content',
+], function($, TwitterContent) {
     var TwitterStreamingContent = function (json) {
-        Content.call(this);
+        TwitterContent.call(this);
         this.tweetId = json.id_str;
         this.author = {
             twitterUserId: json.user.id_str,
             displayName: json.user.screen_name,
-            name: json.user.name
+            name: json.user.name,
+            avatar: json.user.profile_image_url
         };
         this.htmlString = json.text;
-    }
-    $.extend(TwitterStreamingContent.prototype, Content.prototype);
-     
+        this.createdAt = new Date(json.created_at);
+    };
+    TwitterStreamingContent.prototype = new TwitterContent();
+
     return TwitterStreamingContent;
  });
