c0706e4a3f6e251a8cf29b30ea1ba6eb54fb446f
Benjamin Goering <bengoering@gmail.com>
2013-04-02 08:14:36 -0700

    Don't use abs path for requirejs.conf.js paths

--- requirejs.conf.js
+++ requirejs.conf.js
@@ -1,17 +1,17 @@
 require.config({
   baseUrl: '/',
   paths: {
-    jquery: '/lib/jquery/jquery',
-    text: '/lib/requirejs-text/text',
-    base64: '/lib/base64/base64',
-    mustache: '/lib/mustache/mustache',
-    jasmine: '/lib/jasmine/lib/jasmine-core/jasmine',
-    'jasmine-html': '/lib/jasmine/lib/jasmine-core/jasmine-html',
-    'jasmine-jquery': '/lib/jasmine-jquery/lib/jasmine-jquery'
+    jquery: 'lib/jquery/jquery',
+    text: 'lib/requirejs-text/text',
+    base64: 'lib/base64/base64',
+    mustache: 'lib/mustache/mustache',
+    jasmine: 'lib/jasmine/lib/jasmine-core/jasmine',
+    'jasmine-html': 'lib/jasmine/lib/jasmine-core/jasmine-html',
+    'jasmine-jquery': 'lib/jasmine-jquery/lib/jasmine-jquery'
   },
   packages: [{
      name: "streamhub-sdk",
-     location: "/src/"
+     location: "src/"
   }],
   shim: {
     jquery: {
