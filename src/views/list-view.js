25b7347dd5e5e07bbe468b3257055fa433ab0517
Benjamin Goering <bengoering@gmail.com>
2013-04-10 01:50:12 -0700

    ListView is defined using StreamView.extend

--- src/views/list-view.js
+++ src/views/list-view.js
@@ -2,8 +2,8 @@
  * A module to render lists of Content
  * @module streamhub-sdk/views/list-view
  */
-define(['jquery', 'streamhub-sdk/views/stream-view', 'streamhub-sdk/content/views/content-view'],
-function($, StreamView, ContentView) {
+define(['jquery', 'streamhub-sdk/views/stream-view'],
+function($, StreamView) {
     
     /**
      * A simple View that displays Content in a list (`<ul>` by default).
@@ -13,42 +13,40 @@ function($, StreamView, ContentView) {
      * @param opts.el {HTMLElement} The element in which to render the streamed content
      * @constructor
      */
-    var ListView = function(opts) {
-        opts = opts || {};
-        StreamView.call(this, opts);
+    var ListView = StreamView.extend({
+        initialize: function () {
+            var self = this;
+            StreamView.prototype.initialize.apply(this, arguments);
 
-        this.contentViews = [];
+            this.contentViews = [];
+            self.on('add', function(content, stream) {
+                self.add(content, stream);
+            });
+        },
 
-        var self = this;
-        self.on('add', function(content, stream) {
-            self.add(content, stream);
-        });
-    };
-    $.extend(ListView.prototype, StreamView.prototype);
+        elTag: 'ul',
+        elClass: 'content-list',
 
-    // Used if ListView needs to create its own DOMElement
-    ListView.prototype.elTag = 'ul';
-    ListView.prototype.elClass = 'content-list';
+        /**
+         * Add a piece of Content to the ListView
+         *     .createContentView(content)
+         *     add newContentView to this.contentViews[]
+         *     render the newContentView
+         *     insert the newContentView into this.el
+         * @param content {Content} A Content model to add to the ListView
+         * @return the newly created ContentView
+         */
+        add: function(content, stream) {
+            var newContentView = this.createContentView(content);
 
-    /**
-     * Add a piece of Content to the ListView
-     *     .createContentView(content)
-     *     add newContentView to this.contentViews[]
-     *     render the newContentView
-     *     insert the newContentView into this.el
-     * @param content {Content} A Content model to add to the ListView
-     * @return the newly created ContentView
-     */
-    ListView.prototype.add = function(content, stream) {
-        var newContentView = this.createContentView(content);
+            this.contentViews.push(newContentView);
+            newContentView.render();
 
-        this.contentViews.push(newContentView);
-        newContentView.render();
+            $(newContentView.el).prependTo(this.$el);
 
-        $(newContentView.el).prependTo(this.$el);
-
-        return newContentView;
-    };
+            return newContentView;
+        }
+    });
 
     return ListView;
 });
