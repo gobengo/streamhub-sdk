84f390670c0e20e08f231c977d44217ad27c27a0
Gene Hallman <gene@livefyre.com>
2013-04-24 16:04:15 -0700

    LF-5038: Expose all final ContentTypes as Hub.ContentTypes.*

--- src/main.js
+++ src/main.js
@@ -1,58 +1,96 @@
 define([
     'streamhub-sdk/auth',
-    'streamhub-sdk/stream',
     'streamhub-sdk/event-emitter',
-    'streamhub-sdk/content/content',
+    'streamhub-sdk/storage',
+    'streamhub-sdk/stream',
+    'streamhub-sdk/stream-manager',
+    'streamhub-sdk/util',
     'streamhub-sdk/view',
     'streamhub-sdk/clients/livefyre-stream-client',
     'streamhub-sdk/clients/livefyre-bootstrap-client',
     'streamhub-sdk/clients/livefyre-write-client',
+    'streamhub-sdk/content/content',
+    'streamhub-sdk/content/types/livefyre-content',
+    'streamhub-sdk/content/types/livefyre-facebook-content',
+    'streamhub-sdk/content/types/livefyre-twitter-content',
+    'streamhub-sdk/content/types/oembed',
+    'streamhub-sdk/content/types/twitter-content',
+    'streamhub-sdk/content/types/twitter-search-content',
+    'streamhub-sdk/content/types/twitter-streaming-content',
+    'streamhub-sdk/content/views/content-view',
+    'streamhub-sdk/content/views/facebook-content-view',
+    'streamhub-sdk/content/views/twitter-content-view',
     'streamhub-sdk/streams/livefyre-stream',
     'streamhub-sdk/streams/livefyre-reverse-stream',
-    'streamhub-sdk/stream-manager',
     'streamhub-sdk/stream-helpers/livefyre-helper',
-    'streamhub-sdk/content/types/livefyre-content',
     'streamhub-sdk/views/list-view',
     'streamhub-sdk/views/media-wall-view',
     'text!streamhub-sdk/version.txt'
 ], function(
     Auth,
-    Stream,
     EventEmitter,
-    Content,
+    Storage,
+    Stream,
+    StreamManager,
+    Util,
     View,
     LivefyreStreamClient,
     LivefyreBootstrapClient,
     LivefyreWriteClient,
+    Content,
+    LivefyreContent,
+    LivefyreFacebookContent,
+    LivefyreTwitterContent,
+    Oembed,
+    TwitterContent,
+    TwitterSearchContent,
+    TwitterStreamingContent,
+    ContentView,
+    FacebookContentView,
+    TwitterContentView,
     LivefyreStream,
     LivefyreReverseStream,
-    StreamManager,
-    LivefyreStreamHelper,
-    LivefyreContent,
+    LivefyreHelper,
     ListView,
     MediaWallView,
     VersionInfo
 ) {
     var Hub = {};
     Hub.Auth = Auth;
-    Hub.Stream = Stream;
     Hub.EventEmitter = EventEmitter;
-    Hub.Content = Content;
+    Hub.Storage = Storage;
+    Hub.Stream = Stream;
+    Hub.StreamManager = StreamManager;
+    Hub.Util = Util;
     Hub.View = View;
-
+    
     Hub.Clients = {};
     Hub.Clients.LivefyreStreamClient = LivefyreStreamClient;
     Hub.Clients.LivefyreBootstrapClient = LivefyreBootstrapClient;
     Hub.Clients.LivefyreWriteClient = LivefyreWriteClient;
 
+    Hub.Content = Content;
+
+    Hub.Content.Types = {};
+    Hub.Content.Types.LivefyreContent = LivefyreContent;
+    Hub.Content.Types.LivefyreFacebookContent = LivefyreFacebookContent;
+    Hub.Content.Types.LivefyreTwitterContent = LivefyreTwitterContent;
+    Hub.Content.Types.Oembed = Oembed;
+    Hub.Content.Types.TwitterContent = TwitterContent;
+    Hub.Content.Types.TwitterSearchContent = TwitterSearchContent;
+    Hub.Content.Types.TwitterStreamingContent = TwitterStreamingContent;
+
+    Hub.Content.Views = {};
+    Hub.Content.Views.ContentView = ContentView;
+    Hub.Content.Views.FacebookContentView = FacebookContentView;
+    Hub.Content.Views.TwitterContentView = TwitterContentView;
+    
     Hub.Streams = {};
     Hub.Streams.LivefyreStream = LivefyreStream;
     Hub.Streams.LivefyreReverseStream = LivefyreReverseStream;
-
-    Hub.StreamManager = StreamManager;
-
-    Hub.ContentTypes = {};
-    Hub.ContentTypes.LivefyreContent = LivefyreContent;
+    
+    Hub.Streams.Helpers = {};
+    Hub.Streams.Helpers.LivefyreHelper = LivefyreHelper
 
     Hub.Views = {};
     Hub.Views.ListView = ListView;
