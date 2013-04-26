6ed88412279d34462954d0c47f1220eddcd5e55a
Benjamin Goering <bengoering@gmail.com>
2013-04-26 06:20:56 -0700

    Oembed jsdoc

--- src/content/types/oembed.js
+++ src/content/types/oembed.js
@@ -1,7 +1,12 @@
 define(['jquery', 'streamhub-sdk/content/content'], function($, Content) {
 
+    /**
+     * An Oembed Object. As specified by http://oembed.com/
+     * @param json {Object} An Oembed-valid Object
+     */
     var Oembed = function(json) {
         Content.call(this);
+        // @todo set a .body property that can be used to embed standard HTML easily
 
         if (json && Oembed.types.indexOf(json.type) === -1) {
             throw new Error("Oembeds must be constructed with .type in "+Oembed.types);
@@ -15,10 +20,9 @@ define(['jquery', 'streamhub-sdk/content/content'], function($, Content) {
             this.html = null;
         }
     };
+    $.extend(Oembed.prototype, Content.prototype);
 
     Oembed.types = ['photo', 'video', 'link', 'rich'];
-
-    $.extend(Oembed.prototype, Content.prototype);
 
     return Oembed;
  });
