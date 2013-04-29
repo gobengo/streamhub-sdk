491dfa22d0fd06abe12bc89d75107179ad144b37
Gene Hallman <gene@livefyre.com>
2013-04-29 16:39:28 -0700

    fixed build ext == undefined

--- tools/build_version.js
+++ tools/build_version.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 if (process.env.BUILD_NUMBER) {
     var component = JSON.parse(fs.readFileSync('component.json'));
 
-    component.version = component.version + process.env.BUILD_EXT;
+    component.version = component.version + (process.env.BUILD_EXT || "");
 
     fs.writeFileSync('component.json', JSON.stringify(component, null, "  "));
 }
